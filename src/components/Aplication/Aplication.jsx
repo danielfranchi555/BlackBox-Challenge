64c7e562ba800a6aa6a71973ee8bdd30173817a3
danielfranchi <danielfranchi3@gmail.com>
2023-05-08 15:42:00 -0300

    commit

---
+++ src/components/Aplication/Aplication.jsx
@@ -0,0 +1,102 @@
+import { Badge, Button, Center, Image, Stack, Text } from "@chakra-ui/react";
+import React from "react";
+import logo from "../../img/Logo-BlackBox.png";
+import { UsarContext } from "../Context/Context";
+import { motion } from "framer-motion";
+import { Question } from "../Questions/Question";
+import { Answers } from "../Answers/Answers";
+import { Temporizador } from "../Temporizador/Temporizador";
+
+export const Aplication = () => {
+
+//DATOS Y FUNCIONES DEL CONTEXT
+  const {
+    position,
+    data,
+    question,
+    textoEntrada,
+    getTranslate,
+    selectAnswer,
+    allAnswers,
+    setPosition,
+    point,
+    resetGame,
+  } = UsarContext();
+
+  return (
+    <Stack justify="center" align="center" h="600px">
+      <Stack
+        borderRadius="20px"
+        justify="center"
+        align="center"
+        bg="#263849"
+        w={{ base: "300px", md: "400px" }}
+        h="auto"
+      >
+        {position >= data.length ? (
+          <Stack p="20px">
+            <Text fontSize="xl" color="white">
+              Respuestas Correctas : {point}/10
+            </Text>
+            <Button bg="cyan" onClick={() => resetGame()}>
+              Volver a jugar
+            </Button>
+          </Stack>
+        ) : (
+          <Stack
+            as={motion.div}
+            initial={{ scale: 0 }}
+            animate={{ scale: 0.9 }}
+            position="asbsolute"
+            w="400px"
+            justify="center"
+            align="center"
+          >
+            <Stack spacing={0} align="center" direction="row">
+              <Text fontSize="16px" color="white">
+                BlackBox Vision{" "}
+              </Text>
+              <Image
+                boxSize="40px"
+                objectFit="cover"
+                src={logo}
+                alt="Dan Abramov"
+              />
+            </Stack>
+            <Badge
+              borderRadius="10px"
+              w={{ base: "300px", md: "400px" }}
+              variant="solid"
+              bg="#41506b"
+            >
+              <Center p="10px" fontSize="10px" color="white">
+                Category / {question ? question.category : null}
+              </Center>
+            </Badge>
+            <Question
+              textoEntrada={textoEntrada}
+              getTranslate={getTranslate}
+              question={question}
+              position={position}
+            />
+            <Stack>
+              <Badge variant="outline" colorScheme="gray">
+                Difficulty -{" "}
+                <span style={{ color: "cyan" }}>
+                  {question ? question.difficulty : null}
+                </span>
+              </Badge>
+            </Stack>
+            <Stack h="300px" width={{ base: "300px", md: "400px" }}>
+              <Answers selectAnswer={selectAnswer} allAnswers={allAnswers} />
+              <Temporizador
+                setPosition={setPosition}
+                position={position}
+              ></Temporizador>
+            </Stack>
+          </Stack>
+        )}
+      </Stack>
+    </Stack>
+  );
+};
--- src/components/Context/Context.jsx
+++ src/components/Context/Context.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { Box, Center, Spinner, useToast } from "@chakra-ui/react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { useContext, createContext } from "react";
 
@@ -8,9 +9,100 @@ export const UsarContext = () => useContext(ContextState);
 
 export const Context = ({ children }) => {
   const [temp, setTemp] = useState(100);
+  const [data, setData] = useState([]);
+  const [position, setPosition] = useState(0);
+  const [correct, setCorrect] = useState(false);
+  const [textoEntrada, setTextoEntrada] = useState("");
+  const [point, setPoint] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  const question = data[position];
+
+
+  const toast = useToast();
+
+  const getApi = async () => {
+    const data = await fetch("https://opentdb.com/api.php?amount=10");
+    const resp = await data.json();
+    setData(resp.results);
+    setLoading(false);
+  };
+
+
+
+  const getTranslate = async (question) => {
+    const url = `https://api.mymemory.translated.net/get?q=${question.toLowerCase()}&langpair=en|es`;
+    const data = await fetch(url);
+    const resp = await data.json();
+    if (
+      resp.responseData.translatedText ===
+      "INVALID LANGUAGE PAIR SPECIFIED. EXAMPLE: LANGPAIR=EN|IT USING 2 LETTER ISO OR RFC3066 LIKE ZH-CN. ALMOST ALL LANGUAGES SUPPORTED BUT SOME MAY HAVE NO CONTENT"
+    ) {
+      alert("no se puede traducir");
+    } else {
+      setTextoEntrada(resp.responseData.translatedText);
+    }
+  };
+
+
+  useEffect(() => {
+    getApi();
+  }, []);
+
+  const selectAnswer = (text) => {
+    if (text === question.correct_answer) {
+      setPoint(point + 1);
+      toast({
+        position: "bottom",
+        duration: 800,
+        render: () => (
+          <Box color="white" p={3} bg="cyan">
+            Answer Correct!!
+          </Box>
+        ),
+      });
+      setTemp(100)
+      setPosition(position + 1);
+      console.log(point);
+    } else {
+      toast({
+        position: "bottom",
+        duration: 700,
+        render: () => (
+          <Box color="white" p={3} bg="red.500">
+            Answer Incorrect!!
+          </Box>
+        ),
+      });
+      setTemp(100)
+      setCorrect(false);
+      setPosition(position + 1);
+      console.log("no sumaste nada");
+    }
+  };
+
+  const resetGame = ()=>{
+    setPosition(0)
+    setPoint(0)
+  }
+
+  const allAnswers = [
+    question ? question.incorrect_answers : null,
+    question ? question.correct_answer : null,
+  ].flat();
+
+  if (loading) {
+    return (
+      <Center mt="200px">
+        <Spinner size="xl" color="red.500" />
+      </Center>
+    );
+  }
+
+
 
   return (
-    <ContextState.Provider value={{ setTemp, temp }}>
+    <ContextState.Provider value={{ setTemp, temp,position,data,question,textoEntrada,getTranslate,selectAnswer,allAnswers,setPosition,point,resetGame}}>
       {children}
     </ContextState.Provider>
   );
